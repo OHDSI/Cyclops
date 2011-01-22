c9cf1473c266aef254990ff3d6227015fdf3623b
Coki Ta <cokibeach@gmail.com>
2011-01-22 01:30:10 +0000

    git-svn-id: http://bsccs.googlecode.com/svn/branches/coki-branch@30 f7c91f03-731f-d806-b965-e12744d8cf89

---
+++ C/codebase/GPU/GPUDataPartition.h
@@ -0,0 +1,18 @@
+/*
+ * GPUDataPartition.h
+ *
+ *  Created on: Jan 21, 2011
+ *      Author: qta
+ */
+
+#ifndef GPUDATAPARTITION_H_
+#define GPUDATAPARTITION_H_
+
+class GPUDataPartition {
+private:
+	GPUInterface gpu;
+	KernelLauncherCCD kernels;
+
+};
+
+#endif /* GPUDATAPARTITION_H_ */
