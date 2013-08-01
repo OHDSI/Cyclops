d92e4c1c95f535b5a05c1b07609b6e8c71858e7f
Trevor Shaddox <tshaddox@ucla.edu>
2013-08-01 01:18:45 +0000

    with correct files
    git-svn-id: http://bsccs.googlecode.com/svn/branches/full_Bayesian@254 f7c91f03-731f-d806-b965-e12744d8cf89

---
+++ C/codebase/CCD/RandomWalk.cpp
@@ -0,0 +1,114 @@
+/*
+ * RandomWalk.cpp
+ *
+ *  Created on: Jul 31, 2013
+ *      Author: tshaddox
+ */
+
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cmath>
+#include <cstdlib>
+#include <cstring>
+#include <map>
+#include <ctime>
+#include <set>
+
+#include <math.h>
+
+#include "RandomWalk.h"
+#include <Eigen/Dense>
+#include <Eigen/Cholesky>
+#include <Eigen/Core>
+
+
+#include <boost/random.hpp>
+#include <boost/random/normal_distribution.hpp>
+
+#define PI	3.14159265358979323851280895940618620443274267017841339111328125
+
+//#define Debug_TRS
+
+namespace bsccs {
+
+RandomWalk::RandomWalk() {
+
+}
+
+
+RandomWalk::~RandomWalk() {
+
+}
+
+double RandomWalk::getTransformedTuningValue(double tuningParameter){
+	return exp(-tuningParameter);
+}
+void RandomWalk::sample(Model& model, double tuningParameter, boost::mt19937& rng) {
+	cout << "RandomWalk::sample" << endl;
+
+	Parameter & Beta = model.getBeta();
+	Parameter & Beta_Hat = model.getBeta_Hat();
+	Eigen::LLT<Eigen::MatrixXf> choleskyEigen = model.getCholeskyLLT();
+//	Beta->store();
+	int sizeOfSample = Beta.getSize();
+
+
+	vector<bsccs::real> independentNormal;  //Sampled independent normal values
+
+	boost::normal_distribution<> nd(0.0, 1.0); // TODO Construct once
+
+	boost::variate_generator<boost::mt19937&,
+	                           boost::normal_distribution<> > var_nor(rng, nd); // TODO Construct once
+
+	Eigen::VectorXf b = Eigen::VectorXf::Random(sizeOfSample);
+	for (int i = 0; i < sizeOfSample; i++) {
+		bsccs::real normalValue = var_nor();
+		// NB: tuningParameter scales the VARIANCE
+		b[i] = normalValue * std::sqrt(getTransformedTuningValue(tuningParameter)); // multiply by stdev
+	}
+
+#ifdef Debug_TRS
+	cout << "Cholesky in Sampler " << endl;
+	Eigen::MatrixXf CholeskyDecompL(sizeOfSample, sizeOfSample);
+	CholeskyDecompL = choleskyEigen.matrixU();
+	cout << CholeskyDecompL << endl;
+#endif
+
+	(choleskyEigen.matrixU()).solveInPlace(b);
+
+	// TODO Check marginal variance on b[i]
+
+
+	for (int i = 0; i < sizeOfSample; i++) {
+		Beta.set(i, b[i] + Beta_Hat.get(i));
+	}
+
+}
+
+bool RandomWalk::evaluateSample(Model& model, double tuningParameter, boost::mt19937& rng, CyclicCoordinateDescent & ccd){
+	cout << "RandomWalk::evaluateSample" << endl;
+
+
+
+	Parameter & Beta = model.getBeta();
+	Parameter & Beta_Hat = model.getBeta_Hat();
+
+	MHRatio MHstep(ccd);
+	bool accept = MHstep.evaluate(model.getBeta(), model.getBeta_Hat(), model.getSigmaSquared(), ccd, rng, model.getHessian(), tuningParameter);
+
+	if(accept) {
+		for (int i = 0; i < Beta.getSize(); i++) {
+			Beta_Hat.set(i, Beta.get(i));
+		}
+	}
+
+	return(accept);
+}
+
+
+
+
+}
+
---
+++ C/codebase/CCD/RandomWalk.h
@@ -0,0 +1,65 @@
+/*
+ * RandomWalk.h
+ *
+ *  Created on: Jul 31, 2013
+ *      Author: tshaddox
+ */
+
+#ifndef RANDOMWALK_H_
+#define RANDOMWALK_H_
+
+
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cmath>
+#include <cstdlib>
+#include <cstring>
+#include <map>
+#include <time.h>
+#include <set>
+
+#include "Parameter.h"
+#include "TransitionKernel.h"
+#include "MHRatio.h"
+
+
+#include <Eigen/Dense>
+#include <Eigen/Cholesky>
+
+#include "CyclicCoordinateDescent.h"
+#include "CrossValidationSelector.h"
+#include "AbstractSelector.h"
+#include "ccd.h"
+
+#include <boost/random.hpp>
+
+
+namespace bsccs {
+
+class RandomWalk : public TransitionKernel {
+
+public:
+
+	RandomWalk();
+
+	virtual ~RandomWalk();
+
+	void sample(Model& model, double tuningParameter, boost::mt19937& rng);
+
+	bool evaluateSample(Model& model, double tuningParameter, boost::mt19937& rng, CyclicCoordinateDescent& ccd);
+
+	double getTransformedTuningValue(double tuningParameter);
+
+protected:
+
+
+
+};
+
+}
+
+
+
+#endif /* RANDOMWALK_H_ */
