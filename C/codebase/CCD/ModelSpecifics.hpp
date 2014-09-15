1da5c95df7f726d677a9674d2ccf0e683d49c77b
Trevor Shaddox <tshaddox@ucla.edu>
2014-09-15 16:50:08 -0700

    Model Specifics with Yuxi's big num

--- C/codebase/CCD/ModelSpecifics.hpp
+++ C/codebase/CCD/ModelSpecifics.hpp
@@ -199,16 +199,17 @@ double ModelSpecifics<BaseModel,WeightType>::getLogLikelihood(bool useCrossValid
 				logLikelihood -= BaseModel::logLikeDenominatorContrib(hNWeight[i], accDenomPid[i]);
 			}
 		} else {  // TODO Unnecessary code duplication
-/*
+
 			for (int i = 0; i < N; i++) {
 				// Weights modified in computeNEvents()
 				logLikelihood -= BaseModel::logLikeDenominatorContrib(hNWeight[i], denomPid[i]);
 			}
-			*/
+			/*
 			double fakegradient = 0;
 			double fakehessian = 123456;
 			//computeGradientAndHessianImpl<DenseIterator>(0, &fakegradient, &fakehessian, unweighted);
 			logLikelihood -= fakegradient;
+			*/
 		}
 	}
 
@@ -299,72 +300,187 @@ void ModelSpecifics<BaseModel,WeightType>::computeGradientAndHessian(int index, 
 	}
 }
 
-
-template <typename UIteratorType, typename SparseIteratorType>
-std::vector<double> computeHowardRecursion(UIteratorType itExpXBeta, SparseIteratorType itX, int numSubjects, int numCases, bsccs::real* caseOrNo) {
-	// Recursion by Susanne Howard in Gail, Lubin and Rubinstein (1981)
-	// Rewritten as a loop since very deep recursion can be expensive
+class bigNum {
+public:
+	int exp;
+	double frac;
+	union DoubleInt {
+		unsigned i[2];
+		double d;
+	};
+	bigNum (double a) {
+		frac = frexp(a,&exp);
+	}
+	bigNum (double f, int e) {
+		frac = f;
+		exp = e;
+	}
+	static bigNum mul (bigNum a, bigNum b) {
+		if (a.isZero() || b.isZero()) {
+			return bigNum(0);
+		}
+		int e = 0;
+		double f = a.frac * b.frac;
+		if (f < 0.5) {
+			e = -1;
+			f = 2*f;
+		}
+		e = e + a.exp + b.exp;
+		return bigNum(f,e);
+	}
+	static bigNum mul (double a, bigNum b) {
+		if (a==0 || b.isZero()) {
+			return bigNum(0);
+		}
+		int e;
+		double f = frexp(a * b.frac, &e);
+		e = e + b.exp;
+		return bigNum(f,e);
+	}
+	static bigNum div (bigNum a, bigNum b) {
+		if (a.isZero()) {
+			return bigNum(0);
+		}
+		int e = 0;
+		double f = a.frac/b.frac;
+		if (f >= 1) {
+			e = 1;
+			f = f/2;
+		}
+		e = e + a.exp - b.exp;
+		return bigNum(f,e);
+	}
 /*
-	double caseSum = 0;
-
-		std::vector<double> B[2];
-		std::vector<double> dB[2];
-		std::vector<double> ddB[2];
-
-		int currentB = 0;
-
-		B[0].push_back(0);
-		B[1].push_back(0);
-		dB[0].push_back(-100);
-		dB[1].push_back(-100);
-		ddB[0].push_back(-100);
-		ddB[1].push_back(-100);
-
-		for (int i=1; i<=numCases; i++) {
-			B[0].push_back(-100);
-			B[1].push_back(-100);
-			dB[0].push_back(-100);
-			dB[1].push_back(-100);
-			ddB[0].push_back(-100);
-			ddB[1].push_back(-100);
+	static bigNum add (bigNum a, bigNum b) {
+		int n = b.exp - a.exp;
+		if (n > 100) {
+			return b;
 		}
-		double maxXi = 0.0;
-
-		for (int n=1; n<= numSubjects; n++) {
-			for (int m=std::max(1,n+numCases-numSubjects); m<=std::min(n,numCases);m++) {
-
-
-				  B[!currentB][m] =   B[currentB][m] + log(1+exp(log(*itExpXBeta) + B[currentB][m-1] - B[currentB][m]));
-				 dB[!currentB][m] =  dB[currentB][m] + log(1+exp(log(*itExpXBeta) + dB[currentB][m-1] - dB[currentB][m])
-													 + exp(log(*itX) + log(*itExpXBeta) + B[currentB][m-1] - dB[currentB][m]));
-				ddB[!currentB][m] = ddB[currentB][m] + log(1+exp(log(*itExpXBeta) + ddB[currentB][m-1] - ddB[currentB][m])
-													 + exp(2*log(*itX) + log(*itExpXBeta) + B[currentB][m-1] - ddB[currentB][m])
-													 + exp(log(2) + log(*itX) + log(*itExpXBeta) + dB[currentB][m-1] - ddB[currentB][m]));
-
-
-
-			}
-			if (caseOrNo[n-1] == 1) {
-				caseSum += (*itX);
-			}
-			//if (*itX > maxXi) {
-			//	maxXi = *itX;
-			//}
-			currentB = !currentB;
-			++itExpXBeta; ++itX;
+		if (n < -100) {
+			return a;
 		}
-		std::vector<double> result;
-		result.push_back(B[currentB][numCases]);
-		result.push_back(dB[currentB][numCases]);
-		result.push_back(ddB[currentB][numCases]);
-		//result.push_back(1);
-		result.push_back(caseSum);
-		result.push_back(maxXi);
+		int e;
+		double f = frexp(a.frac * pow(2, a.exp-b.exp) + b.frac, &e);
+		e = e + b.exp;
 
+		printf("%f \n", bigNum(f,e).toDouble());
+		printf("%f \n", bigNum::add1(a,b).toDouble());
+		printf("%f %d %f %d \n", a.frac, a.exp, b.frac, b.exp);
 
-		return result;
+		return bigNum(f,e);
+	}
 */
 
+	static bigNum add(bigNum a, bigNum b) {
+		int n = b.exp - a.exp;
+		if (n > 100 || a.isZero()) {
+			return b;
+		}
+		if (n < -100 || b.isZero()) {
+			return a;
+		}
+		DoubleInt x;
+		x.d = b.frac;
+		x.i[1] = (((x.i[1]>>20)+n)<<20)|((x.i[1]<<12)>>12);
+		int e;
+		double f = frexp(x.d + a.frac, &e);
+		e = e + a.exp;
+		return bigNum(f,e);
+	}
+
+	bool isZero() {
+		return (frac==0 && exp==0);
+	}
+
+
+	double toDouble() {
+		return frac*pow(2,exp);
+	}
+};
+template <typename UIteratorType, typename SparseIteratorType>
+//std::vector<bigNum> computeHowardRecursion(UIteratorType itExpXBeta, SparseIteratorType itX, int numSubjects, int numCases, bsccs::real* caseOrNo) {
+std::vector<double> computeHowardRecursion(UIteratorType itExpXBeta, SparseIteratorType itX, int numSubjects, int numCases, bsccs::real* caseOrNo) {
+
+	// Recursion by Susanne Howard in Gail, Lubin and Rubinstein (1981)
+	// Rewritten as a loop since very deep recursion can be expensive
+
+/*
+ // bigNum code
+
+	double caseSum = 0;
+
+	std::vector<bigNum> B[2];
+	std::vector<bigNum> dB[2];
+	std::vector<bigNum> ddB[2];
+
+	int currentB = 0;
+
+	B[0].push_back(bigNum(1));
+	B[1].push_back(bigNum(1));
+	dB[0].push_back(bigNum(0));
+	dB[1].push_back(bigNum(0));
+	ddB[0].push_back(bigNum(0));
+	ddB[1].push_back(bigNum(0));
+
+	for (int i=1; i<=numCases; i++) {
+		B[0].push_back(bigNum(0));
+		B[1].push_back(bigNum(0));
+		dB[0].push_back(bigNum(0));
+		dB[1].push_back(bigNum(0));
+		ddB[0].push_back(bigNum(0));
+		ddB[1].push_back(bigNum(0));
+	}
+	//double maxXi = 0.0;
+	//double maxSorted = 0.0;
+	bigNum temp(0);
+	//std::vector<double> sortXi;
+	for (int n=1; n<= numSubjects; n++) {
+		for (int m=std::max(1,n+numCases-numSubjects); m<=std::min(n,numCases);m++) {
+			  B[!currentB][m] = bigNum::add(B[currentB][m], bigNum::mul(*itExpXBeta, B[currentB][m-1]));
+			//B[!currentB][m] =   B[currentB][m] + (*itExpXBeta) *   B[currentB][m-1];
+
+			  	 temp = bigNum::add(dB[currentB][m], bigNum::mul(*itExpXBeta, dB[currentB][m-1]));
+			 dB[!currentB][m] = bigNum::add(temp, bigNum::mul((*itX)*(*itExpXBeta), B[currentB][m-1]));
+			//dB[!currentB][m] =  dB[currentB][m] + (*itExpXBeta) *  dB[currentB][m-1] + (*itX)*(*itExpXBeta)*B[currentB][m-1];
+
+			 	 temp = bigNum::add(ddB[currentB][m], bigNum::mul(*itExpXBeta, ddB[currentB][m-1]));
+			 	 temp = bigNum::add(temp, bigNum::mul((*itX)*(*itX)*(*itExpXBeta), B[currentB][m-1]));
+			ddB[!currentB][m] = bigNum::add(temp, bigNum::mul(2*(*itX)*(*itExpXBeta), dB[currentB][m-1]));
+			//ddB[!currentB][m] = ddB[currentB][m] + (*itExpXBeta) * ddB[currentB][m-1] + (*itX)*(*itX)*(*itExpXBeta)*B[currentB][m-1] + 2*(*itX)*(*itExpXBeta)*dB[currentB][m-1];
+		}
+		if (caseOrNo[n-1] == 1) {
+			caseSum += (*itX);
+		}
+
+		//if (*itX > maxXi) {
+		//	maxXi = *itX;
+		//}
+		//sortXi.push_back(*itX);
+
+		currentB = !currentB;
+		++itExpXBeta; ++itX;
+
+	}
+
+	//std::sort (sortXi.begin(), sortXi.end());
+	//for (int i=1; i<=numCases; i++) {
+	//	maxSorted += sortXi[numSubjects-i];
+	//}
+	//maxXi = maxXi * numCases;
+
+
+	std::vector<bigNum> result;
+	result.push_back(B[currentB][numCases]);
+	result.push_back(dB[currentB][numCases]);
+	result.push_back(ddB[currentB][numCases]);
+	result.push_back(bigNum(caseSum));
+	//result.push_back(bigNum(maxXi));
+	//result.push_back(maxSorted);
+
+	return result;
+
+	*/
+ // Normal Code
 	double caseSum = 0;
 
 	std::vector<double> B[2];
@@ -400,23 +516,23 @@ std::vector<double> computeHowardRecursion(UIteratorType itExpXBeta, SparseItera
 		if (caseOrNo[n-1] == 1) {
 			caseSum += (*itX);
 		}
-		/*
-		if (*itX > maxXi) {
-			maxXi = *itX;
-		}
-		sortXi.push_back(*itX);
-		*/
+
+		//if (*itX > maxXi) {
+		//	maxXi = *itX;
+		//}
+		//sortXi.push_back(*itX);
+
 		currentB = !currentB;
 		++itExpXBeta; ++itX;
 
 	}
-/*
-	std::sort (sortXi.begin(), sortXi.end());
-	for (int i=1; i<=numCases; i++) {
-		maxSorted += sortXi[numSubjects-i];
-	}
-	maxXi = maxXi * numCases;
-	*/
+
+	//std::sort (sortXi.begin(), sortXi.end());
+	//for (int i=1; i<=numCases; i++) {
+	//	maxSorted += sortXi[numSubjects-i];
+	//}
+	//maxXi = maxXi * numCases;
+
 
 	std::vector<double> result;
 	result.push_back(B[currentB][numCases]);
@@ -427,7 +543,6 @@ std::vector<double> computeHowardRecursion(UIteratorType itExpXBeta, SparseItera
 	//result.push_back(maxSorted);
 
 	return result;
-
 }
 
 
@@ -503,23 +618,28 @@ void ModelSpecifics<BaseModel,WeightType>::computeGradientAndHessianImpl(int ind
 #else
 				DenseView<IteratorType> x(IteratorType(*hXI, index), hNtoK[n], hNtoK[n+1]);
 #endif
+				//std::vector<bigNum> value = computeHowardRecursion(offsExpXBeta + hNtoK[n], x, numSubjects, numCases, hY + hNtoK[n]);
 				std::vector<double> value = computeHowardRecursion(offsExpXBeta + hNtoK[n], x, numSubjects, numCases, hY + hNtoK[n]);
 
+				/*
 				if (hessian == 123456) {
-					//gradient += log(value[0]);
-					gradient += value[0];
+					gradient += log(value[0]);
+					//gradient += log(value[0].toDouble());
 				}
 				else {
-					//real apple = (real)(value[3] - value[1]/value[0]);
-					//real apple = (real) (value[3] - exp(value[1]-value[0]));
+				*/
+				//normal
+				gradient += (real)(value[3] - value[1]/value[0]);
+				hessian += (real)((value[1]*value[1])/(value[0]*value[0]) - value[2]/value[0]);
 
-					//real banana = (real)((value[1]*value[1])/(value[0]*value[0]) - value[2]/value[0]);
-					//real banana = (real)((value[1]*value[1])/(value[0]*value[0]) - value[4]*value[4]);
-					//real banana = (real) (exp(2*(value[1]-value[0])) - exp(value[2]-value[0]));
+				//MM
+				//real banana = (real)((value[1]*value[1])/(value[0]*value[0]) - value[4]*value[4]);
+				//real banana = (real)(pow(bigNum::div(value[1],value[0]).toDouble(), 2) - pow(value[4].toDouble(),2));
 
-					gradient += (real)(value[3] - value[1]/value[0]);
-					hessian += (real)((value[1]*value[1])/(value[0]*value[0]) - value[2]/value[0]);
-				}
+				//bigNum
+				//gradient += (real)(value[3].toDouble() - bigNum::div(value[1],value[0]).toDouble());
+				//hessian += (real)(pow(bigNum::div(value[1],value[0]).toDouble(), 2) - bigNum::div(value[2],value[0]).toDouble());
+
 			} else {
 
 				// Compile-time delegation
