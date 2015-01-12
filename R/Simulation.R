49ebdc414f841cda1e30ea144c9a06a306dff259
Marc Suchard <msuchard@gmail.com>
2015-01-12 09:16:24 -0800

    Small R CMD check warning fix

--- R/Simulation.R
+++ R/Simulation.R
@@ -234,7 +234,7 @@ fitUsingCyclops <- function(sim,
                                               sim$covariates$covariateValue)
                 
             }
-            cyclopsFit <- fitCyclopsModel(dataPtr,forceColdStart=FALSE,prior = createPrior("laplace",0.1,exclude=i))
+            cyclopsFit <- fitCyclopsModel(dataPtr,prior = createPrior("laplace",0.1,exclude=i))
             coefCyclops[i] <- coef(cyclopsFit)[names(coef(cyclopsFit)) == as.character(i)]
             if (coverage) {
                 if (model == "survival"){
