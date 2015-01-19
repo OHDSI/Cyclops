b52b617e6fd8bef66c0579b048cc036943450943
Marc Suchard <msuchard@gmail.com>
2015-01-19 14:15:20 -0800

    Removed templated alias in ThreadPool

--- src/cyclops/engine/ThreadPool.h
+++ src/cyclops/engine/ThreadPool.h
@@ -65,7 +65,8 @@ template<class F, class... Args>
 auto ThreadPool::enqueue(F&& f, Args&&... args) 
     -> std::future<typename std::result_of<F(Args...)>::type>
 {
-    using return_type = typename std::result_of<F(Args...)>::type;
+//     using return_type = typename std::result_of<F(Args...)>::type;
+    typedef typename std::result_of<F(Args...)>::type return_type;
 
     auto task = std::make_shared< std::packaged_task<return_type()> >(
             std::bind(std::forward<F>(f), std::forward<Args>(args)...)
