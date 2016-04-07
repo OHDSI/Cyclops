71411e1a09cd689a120a584d3aa0409b20bfe96e
Marc Suchard <msuchard@gmail.com>
2016-04-07 10:03:29 -0700

    compiling some OpenCL code

--- src/cyclops/engine/GpuModelSpecifics.hpp
+++ src/cyclops/engine/GpuModelSpecifics.hpp
@@ -14,6 +14,59 @@
 
 namespace bsccs {
 
+namespace compute = boost::compute;
+
+namespace detail {
+
+template <typename DeviceVec, typename HostVec>
+DeviceVec allocateAndCopyToDevice(const HostVec& hostVec, compute::context& context, compute::command_queue& queue) {
+    DeviceVec deviceVec(hostVec.size(), context);
+    compute::copy(std::begin(hostVec), std::end(hostVec), std::begin(deviceVec), queue);
+    return std::move(deviceVec);
+}
+
+}; // namespace detail
+
+template <typename RealType>
+class GpuColumn {
+public:
+    typedef compute::vector<RealType> DataVector;
+    typedef compute::vector<int> IndicesVector;
+
+    //GpuColumn(const GpuColumn<RealType>& copy);
+
+    GpuColumn(const CompressedDataColumn& column,
+              compute::context& context,
+              compute::command_queue& queue)
+        : format(column.getFormatType()) {
+
+            // Data vector
+            if (format == FormatType::SPARSE ||
+                format == FormatType::DENSE) {
+                const auto& columnData = column.getDataVector();
+                data = detail::allocateAndCopyToDevice<DataVector>(columnData, context, queue);
+            }
+
+            // Indices vector
+            if (format == FormatType::INDICATOR ||
+                format == FormatType::SPARSE) {
+                const auto& columnIndices = column.getColumnsVector();
+                indices = detail::allocateAndCopyToDevice<IndicesVector>(columnIndices, context, queue);
+            }
+        }
+
+    virtual ~GpuColumn() { }
+
+    const IndicesVector& getIndicesVector() const { return indices; }
+    const DataVector& getDataVector() const { return data; }
+
+private:
+    FormatType format;
+    IndicesVector indices;
+    DataVector data;
+};
+
+
 template <class BaseModel, typename WeightType>
 class GpuModelSpecifics : public ModelSpecifics<BaseModel, WeightType> {
 public:
@@ -25,13 +78,23 @@ public:
 
         // Get device ready to compute
         device = boost::compute::system::find_device(deviceName);
-        // device = boost::compute::system::default_device(); // TODO Generalize
         std::cerr << "Using: " << device.name() << std::endl;
 
         ctx = boost::compute::context{device};
         queue = boost::compute::command_queue{ctx, device
             , boost::compute::command_queue::enable_profiling
         };
+
+        // Copy data
+        for (size_t i = 0; i < input.getNumberOfColumns(); ++i) {
+            columns.emplace_back(GpuColumn<real>(input.getColumn(i), ctx, queue));
+        }
+        auto& inputY = input.getYVectorRef();
+        y = detail::allocateAndCopyToDevice<compute::vector<real> >(inputY, ctx, queue);
+
+        buildAllKernels();
+
+        printAllKernels();
     }
 
     virtual ~GpuModelSpecifics() {
@@ -39,14 +102,58 @@ public:
     }
 
 private:
+
+    void buildGradientHessianKernel() {
+
+        std::stringstream options;
+        options << "-DTILE_DIM=" << 16 << " -DREAL_VECTOR=float -DREAL=float";
+
+        std::stringstream code;
+        // code << "#pragma OPENCL EXTENSION cl_khr_fp64 : enable\n";
+
+        code <<
+            " __kernel void computeSSR(__global const REAL_VECTOR *locations,  \n" <<
+            "  						   __global const REAL *observations,      \n" <<
+            "						   __global REAL *squaredResiduals,        \n" <<
+            "						   const uint locationCount) {             \n" <<
+            " }                                                                \n";
+
+        compute::program program = compute::program::build_with_source(code.str(), ctx, options.str());
+        kernelGradientHessian = compute::kernel(program, "computeSSR");
+
+    }
+
+    void printKernel(compute::kernel& kernel) {
+        auto program = kernel.get_program();
+        auto buildOptions = program.get_build_info<std::string>(CL_PROGRAM_BUILD_OPTIONS, device);
+
+        std::cerr // TODO Change to R
+            << "Options: " << buildOptions << std::endl
+            << program.source()
+            << std::endl;
+    }
+
+    void buildAllKernels() {
+        buildGradientHessianKernel();
+    }
+
+    void printAllKernels() {
+        printKernel(kernelGradientHessian);
+    }
+
     // boost::compute objects
-    boost::compute::device device;
-    boost::compute::context ctx;
-    boost::compute::command_queue queue;
-    boost::compute::program program;
+    compute::device device;
+    compute::context ctx;
+    compute::command_queue queue;
+    compute::program program;
+
+    compute::kernel kernelGradientHessian;
+
+    // vectors of columns
+    std::vector<GpuColumn<real> > columns;
+    compute::vector<real> y;
 };
 
 } // namespace bsccs
 
 #endif /* GPUMODELSPECIFICS_HPP_ */
-//
