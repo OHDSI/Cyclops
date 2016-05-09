aa6d3f1bd27b2324c2966436cb9d907028d0ddfb
Marc Suchard <msuchard@gmail.com>
2016-05-09 15:33:00 -0700

    attempt to fix namespace warnings with RcppParallel

--- R/cyclops.R
+++ R/cyclops.R
@@ -3,13 +3,13 @@
 # Copyright 2014 Observational Health Data Sciences and Informatics
 #
 # This file is part of Cyclops
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -28,10 +28,10 @@
 #' found in large-scale observational healthcare data.  Implementations focus
 #' on computational optimization and fine-scale parallelization to yield
 #' efficient inference in massive datasets.
-#' 
+#'
 #' @docType package
 #' @name cyclops
-#' @import Rcpp Matrix RcppParallel
+#' @import Rcpp Matrix
 #' @useDynLib Cyclops
 NULL
 
