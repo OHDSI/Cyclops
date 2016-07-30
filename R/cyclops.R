fa82fec6b9113640a2ab1fe723a5f674fc342af1
Marc Suchard <msuchard@gmail.com>
2016-07-29 20:57:03 -0700

    minor CRAN check fixes

--- R/cyclops.R
+++ R/cyclops.R
@@ -33,6 +33,10 @@
 #' @name cyclops
 #' @import Rcpp Matrix
 #' @importFrom RcppParallel setThreadOptions
+#'
+#' @importFrom methods as
+#' @importFrom stats aggregate as.formula coef coefficients confint contrasts deviance model.matrix model.offset model.response pchisq poisson qchisq qnorm rbinom rexp rnorm rpois runif terms time vcov
+#'
 #' @useDynLib Cyclops
 NULL
 
