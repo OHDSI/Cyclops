b176f8d90f4fc4efed640479c8e1c49f0fd219c7
Marc Suchard <msuchard@gmail.com>
2016-08-27 07:46:57 -0700

    example of internal simulation for ABRIDGE

--- tests/testthat/test-Abridge.R
+++ tests/testthat/test-Abridge.R
@@ -4,6 +4,25 @@ library("testthat")
 # ABRIDGE regression
 #
 
+test_that("Cyclops simulation routines", {
+    # Generate data
+    set.seed(666)
+    sim <- simulateCyclopsData(nstrata = 1, nrows = 10000, ncovars = 100,
+                        effectSizeSd = 1, zeroEffectSizeProp = 0.9, eCovarsPerRow = 10,
+                        model = "survival")
+    data <- convertToCyclopsData(sim$outcomes,
+                                 sim$covariates,
+                                 modelType = "cox")
+
+    # Fit model
+    abridge <- fitCyclopsModel(data,
+                               prior = createAbridgePrior())
+
+    # Examine estimates vis-a-vis simulation conditions
+    which(log(sim$effectSizes$rr) != 0)
+    which(coef(abridge) != 0)
+})
+
 test_that("ABRIDGE simulated logistic regression", {
     set.seed(666)
     p <- 20
