f86b71b091be911589bf5e79debe64720819d205
jianxiaoyang <yangjx@g.ucla.edu>
2017-09-22 12:44:32 +0800

    modify test-weighting

--- tests/testthat/test-weighting.R
+++ tests/testthat/test-weighting.R
@@ -3,68 +3,64 @@ library("survival")
 
 test_that("Check very small Cox example with ties, but without weights",{
     test <- read.table(header=T, sep = ",", text = "
-                       start, length, status, x1
-                       0, 5, 0, 0
-                       0, 4, 1, 1
-                       0, 3, 0, 0
-                       0, 2, 0, 1
-                       0, 2, 1, 0
-                       0, 2, 1, 0
-                       0, 1, 0, 0
-                       0, 1, 1, 2 ")
+                       start, length, status, x1, x2
+                       0, 5, 0, 0, 1
+                       0, 4, 1, 1, 2
+                       0, 3, 0, 0, 1
+                       0, 2, 0, 1, 0
+                       0, 2, 1, 0, 0
+                       0, 2, 1, 0, 1
+                       0, 1, 0, 0, 0
+                       0, 1, 1, 2, 1 ")
+    tolerance <- 1E-4
 
-    gold <- coxph(Surv(length, status) ~ x1, test, ties = "breslow")
+    gold <- coxph(Surv(length, status) ~ x1 + x2, test, ties = "breslow")
 
-    cyclopsData <- createCyclopsData(Surv(length, status) ~ x1, data = test, modelType = "cox")
+    cyclopsData <- createCyclopsData(Surv(length, status) ~ x1 + x2, data = test, modelType = "cox")
     cyclopsFit <- fitCyclopsModel(cyclopsData)
 
-    expect_equal(coef(gold), coef(cyclopsFit))
+    expect_equal(coef(gold), coef(cyclopsFit), tolerance = tolerance)
     expect_equivalent(logLik(gold),logLik(cyclopsFit))
 
 })
 
-
-<<<<<<< HEAD
 test_that("Check very small Cox example without ties, but with weights",{
     test <- read.table(header=T, sep = ",", text = "
-                   start, length, status, x1
-                       0, 5,  0,0
-                       0, 4,  1,1
-                       0, 3,  0,0
-                       0, 2,  0,1
-                       0, 2,  1,0
-                       0, 1,  0,1
-                       0, 1,  1,1 ")
+                   start, length, status, x1, x2
+                       0, 5,  0,0,1
+                       0, 4,  1,1,2
+                       0, 3,  0,0,1
+                       0, 2,  0,1,0
+                       0, 2,  1,0,0
+                       0, 1,  0,1,0
+                       0, 1,  1,2,1 ")
     weights <- c(1,2,1,2,3,2,1)
 
-    gold <- coxph(Surv(length, status) ~ x1, test, weights = weights)
+    gold <- coxph(Surv(length, status) ~ x1 + x2, test, weights = weights)
 
-    cyclopsData <- createCyclopsData(Surv(length, status) ~ x1, data = test, modelType = "cox")
+    cyclopsData <- createCyclopsData(Surv(length, status) ~ x1 + x2, data = test, modelType = "cox")
     cyclopsFit <- fitCyclopsModel(cyclopsData, weights = weights)
 
     expect_equal(coef(gold), coef(cyclopsFit))
     expect_equivalent(logLik(gold),logLik(cyclopsFit))
 })
 
-
-=======
->>>>>>> parent of 33e737c... try to fix tests (parallel confint hasn't succeeded yet)
 test_that("Check very small Cox example with ties, with weights",{
     test <- read.table(header=T, sep = ",", text = "
-                       start, length, status, x1
-                       0, 5, 0, 0
-                       0, 4, 1, 1
-                       0, 3, 0, 0
-                       0, 2, 0, 1
-                       0, 2, 1, 0
-                       0, 2, 1, 0
-                       0, 1, 0, 0
-                       0, 1, 1, 2 ")
+                       start, length, status, x1, x2
+                       0, 5, 0, 0, 1
+                       0, 4, 1, 1, 2
+                       0, 3, 0, 0, 1
+                       0, 2, 0, 1, 0
+                       0, 2, 1, 0, 0
+                       0, 2, 1, 0, 1
+                       0, 1, 0, 0, 0
+                       0, 1, 1, 2, 1 ")
     weights <- c(1,2,1,2,4,3,2,1)
 
-    gold <- coxph(Surv(length, status) ~ x1, test, weights, ties = "breslow")
+    gold <- coxph(Surv(length, status) ~ x1 + x2, test, weights, ties = "breslow")
 
-    cyclopsData <- createCyclopsData(Surv(length, status) ~ x1, data = test, modelType = "cox")
+    cyclopsData <- createCyclopsData(Surv(length, status) ~ x1 + x2, data = test, modelType = "cox")
     cyclopsFit <- fitCyclopsModel(cyclopsData, weights=weights)
 
     expect_equal(coef(gold), coef(cyclopsFit))
