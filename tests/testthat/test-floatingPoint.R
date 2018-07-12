3ee4ca24bc70860177abb7519be9e55fb99a930d
Marc A. Suchard <msuchard@ucla.edu>
2018-07-12 15:22:44 -0400

    speed difference

--- tests/testthat/test-floatingPoint.R
+++ tests/testthat/test-floatingPoint.R
@@ -52,3 +52,41 @@ test_that("Single precision", {
     expect_equal(coef(cyclopsFitS), coef(glmFit), tolerance = tolerance)
 })
 
+test_that("Speed difference", {
+    skip("for debugging purposes; simulation takes too long")
+
+    set.seed(123)
+    sim <- simulateCyclopsData(nstrata=1,
+                               ncovars=1000,
+                               nrows=100000,
+                               effectSizeSd=0.5,
+                               eCovarsPerRow=2,
+                               model="logistic")
+
+    dataD <- convertToCyclopsData(outcomes = sim$outcomes,
+                                  covariates = sim$covariates, modelType = "lr",
+                                  floatingPoint = 64)
+
+    dataS <- convertToCyclopsData(outcomes = sim$outcomes,
+                                  covariates = sim$covariates, modelType = "lr",
+                                  floatingPoint = 32)
+
+    system.time(
+        fitD <- fitCyclopsModel(cyclopsData = dataD,
+                                prior = createPrior("laplace", variance = 1),
+                                control = createControl(fold = 10, cvRepetitions = 1,
+                                                        startingVariance = 0.1, noiseLevel = "silent",
+                                                        seed = 123),
+                                forceNewObject = TRUE))
+
+    system.time(
+        fitS <- fitCyclopsModel(cyclopsData = dataS,
+                                prior = createPrior("laplace", variance = 1),
+                                control = createControl(fold = 10, cvRepetitions = 1,
+                                                        startingVariance = 0.1, noiseLevel = "silent",
+                                                        seed = 123),
+                                forceNewObject = TRUE))
+
+    expect_equal(coef(fitD)[1:10], coef(fitS)[1:10], tolerance = 1E-3)
+})
+
