d9ede0427f7a7e50aae699f1c921af3cce8aefa9
Marc A. Suchard <msuchard@ucla.edu>
2020-03-22 20:08:18 -0700

    remove broken SCCS web link

--- R/ExampleData.R
+++ R/ExampleData.R
@@ -3,13 +3,13 @@
 # Copyright 2014 Observational Health Data Sciences and Informatics
 #
 # This file is part of Cyclops
-# 
+#
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
-# 
+#
 #     http://www.apache.org/licenses/LICENSE-2.0
-# 
+#
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -22,11 +22,11 @@
 #' Farrington and Whitaker.  There are 10 patients comprising 38 unique exposure intervals.
 #'
 #' @name oxford
-#' 
+#'
 #' @docType data
-#' 
+#'
 #' @usage data(oxford)
-#' 
+#'
 #' @format A data frame with 38 rows and 6 variables:
 #' \describe{
 #'   \item{indiv}{patient identifier}
@@ -37,5 +37,4 @@
 #'   \item{loginterval}{log interval length}
 #'   ...
 #' }
-#' @source \url{http://statistics.open.ac.uk/sccs/r.htm}
 NULL
