4e15b30c2a06d2043ed9386c9209ef49b6b12125
JianxiaoYang <yangjx@ucla.edu>
2023-10-24 10:36:12 -0700

    update non-GPU Makevars

--- tests/testthat/test-timevaryingCox.R
+++ tests/testthat/test-timevaryingCox.R
@@ -5,6 +5,7 @@ GpuDevice <- listOpenCLDevices()[1]
 tolerance <- 1E-4
 
 test_that("Check very small Cox example with time-varying coefficient as stratified model", {
+    skip_if(length(listOpenCLDevices()) == 0, "GPU not available")
     test <- read.table(header=T, sep = ",", text = "
             start, length, event, x1, x2
             0, 4,  1,0,0
