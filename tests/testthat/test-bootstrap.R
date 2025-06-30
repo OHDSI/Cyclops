dc03ce26b02dc5428b233a827a312f19c3db24ed
schuemie <schuemie@ohdsi.org>
2025-06-30 09:00:17 +0200

    Added more bootstrap unit tests (several currently fail)

--- tests/testthat/test-bootstrap.R
+++ tests/testthat/test-bootstrap.R
@@ -1,5 +1,20 @@
 library("testthat")
 library("boot")
+library("Cyclops")
+library("survival")
+
+test_that("Small conditional logistic regression bootstrap", {
+    set.seed(123)
+    dataPtr <- createCyclopsData(case ~ spontaneous + induced + strata(stratum),
+                                 data = infert,
+                                 modelType = "clr")
+
+    cyclopsFit <- fitCyclopsModel(dataPtr, prior = createPrior("none"))
+
+    bs <- runBootstrap(cyclopsFit, replicates = 4999)
+    expect_lt(abs(mean(bs$summary$bias)), 0.001)
+    # Error: abs(mean(bs$summary$bias)) is not strictly less than 0.001. Difference: 0.0808
+})
 
 test_that("Small Poisson bootstrap examples with and without weights", {
 
@@ -80,23 +95,183 @@ test_that("Small Poisson bootstrap examples with an offset", {
     expect_equal(cbo$summary$bpi_lower + c(1,0,0,0,0), cbn$summary$bpi_lower, tolerance = 1E-4)
 })
 
-# test_that("Large Cox with weights and bootstrapping", {
-#     set.seed(123)
-#     sim <- simulateCyclopsData(nstrata=1000,
-#                                ncovars=10,
-#                                nrows=10000,
-#                                effectSizeSd=0.5,
-#                                eCovarsPerRow=2,
-#                                model="survival")
-#     sim$outcomes$weights <- 1/sim$outcomes$rr
-#
-#     # Cyclops
-#     cyclopsData <- convertToCyclopsData(outcomes = sim$outcomes,
-#                                         covariates = sim$covariates,
-#                                         modelType = "cox")
-#     fitCyclops <- fitCyclopsModel(cyclopsData = cyclopsData)
-#
-#     bs <- runBootstrap(fitCyclops, outFileName = "out.txt", treatmentId = "1", replicates = 100)
-#     result <- read.csv("out.txt")
-#     result
-# })
+test_that("Cox example bootstrap with failure ties, strata and data weights", {
+    test <- read.table(header=T, sep = ",", text = "
+start, length, event, x1, x2
+0, 4,  1,0,0
+0, 3,  1,2,0
+0, 3,  0,0,1
+0, 2,  1,0,1
+0, 2,  1,1,1
+0, 1,  0,1,0
+0, 1,  1,1,0
+")
+    test <- rbind(data.frame(test, index = 1:7), data.frame(test, index = 1:7))
+
+    cyclopsData <- createCyclopsData(Surv(length, event) ~ x1 + strata(x2),
+                                     data = test,
+                                     modelType = "cox")
+
+    fit <- fitCyclopsModel(cyclopsData, weights = rep(c(0,1), 7))
+    bootstrap <- runBootstrap(fit, replicates = 4999)
+    # Error in quantile.default(X[[i]], ...) :
+    #     missing values and NaN's not allowed if 'na.rm' is FALSE
+})
+
+test_that("Large logistic bootstrap with and without weights", {
+    set.seed(123)
+    sim <- simulateCyclopsData(nstrata=100,
+                               ncovars=4,
+                               nrows=1000,
+                               effectSizeSd=0.5,
+                               eCovarsPerRow=2,
+                               model="logistic")
+    cyclopsData <- convertToCyclopsData(outcomes = sim$outcomes,
+                                        covariates = sim$covariates,
+                                        modelType = "lr")
+    fitCyclopsNoWeights <- fitCyclopsModel(cyclopsData = cyclopsData)
+    bsNoWeights <- runBootstrap(fitCyclopsNoWeights, replicates = 4999)
+    expect_lt(abs(mean(bsNoWeights$summary$bias)), 0.002)
+    # Error: abs(mean(bsNoWeights$summary$bias)) is not strictly less than 0.002. Difference: 0.000326
+
+    sim$outcomes$weights <- rep(c(0.1,0.9), nrow(sim$outcomes) / 2)
+
+    cyclopsData <- convertToCyclopsData(outcomes = sim$outcomes,
+                                        covariates = sim$covariates,
+                                        modelType = "lr")
+    fitCyclops <- fitCyclopsModel(cyclopsData = cyclopsData)
+    bs <- runBootstrap(fitCyclops, replicates = 4999)
+    expect_lt(abs(mean(bs$summary$bias)), 0.002)
+    # Error: abs(mean(bsNoWeights$summary$bias)) is not strictly less than 0.002. Difference: 0.000326
+})
+
+test_that("Large logistic Poisson bootstrap with and without weights", {
+    set.seed(123)
+    sim <- simulateCyclopsData(nstrata=100,
+                               ncovars=4,
+                               nrows=1000,
+                               effectSizeSd=0.5,
+                               eCovarsPerRow=2,
+                               model="logistic")
+    cyclopsData <- convertToCyclopsData(outcomes = sim$outcomes,
+                                        covariates = sim$covariates,
+                                        modelType = "clr",
+                                        addIntercept = FALSE)
+    fitCyclopsNoWeights <- fitCyclopsModel(cyclopsData = cyclopsData)
+    bsNoWeights <- runBootstrap(fitCyclopsNoWeights, replicates = 1999)
+    expect_lt(abs(mean(bsNoWeights$summary$bias)), 0.002)
+
+    sim$outcomes$weights <- rep(c(0.1,0.9), nrow(sim$outcomes) / 2)
+
+    cyclopsData <- convertToCyclopsData(outcomes = sim$outcomes,
+                                        covariates = sim$covariates,
+                                        modelType = "clr",
+                                        addIntercept = FALSE)
+    fitCyclops <- fitCyclopsModel(cyclopsData = cyclopsData)
+    bs <- runBootstrap(fitCyclops, replicates = 1999)
+    expect_lt(abs(mean(bs$summary$bias)), 0.002)
+})
+
+test_that("Large Poisson bootstrap with and without weights", {
+    set.seed(123)
+    sim <- simulateCyclopsData(nstrata=100,
+                               ncovars=4,
+                               nrows=1000,
+                               effectSizeSd=0.5,
+                               eCovarsPerRow=2,
+                               model="poisson")
+    cyclopsData <- convertToCyclopsData(outcomes = sim$outcomes,
+                                        covariates = sim$covariates,
+                                        modelType = "pr")
+    fitCyclopsNoWeights <- fitCyclopsModel(cyclopsData = cyclopsData)
+    bsNoWeights <- runBootstrap(fitCyclopsNoWeights, replicates = 1999)
+    expect_lt(abs(mean(bsNoWeights$summary$bias)), 0.002)
+
+    sim$outcomes$weights <- rep(c(0.1,0.9), nrow(sim$outcomes) / 2)
+
+    cyclopsData <- convertToCyclopsData(outcomes = sim$outcomes,
+                                        covariates = sim$covariates,
+                                        modelType = "pr")
+    fitCyclops <- fitCyclopsModel(cyclopsData = cyclopsData)
+    bs <- runBootstrap(fitCyclops, replicates = 1999)
+    expect_lt(abs(mean(bs$summary$bias)), 0.002)
+})
+
+test_that("Large conditional Poisson bootstrap with and without weights", {
+    set.seed(123)
+    sim <- simulateCyclopsData(nstrata=100,
+                               ncovars=4,
+                               nrows=1000,
+                               effectSizeSd=0.5,
+                               eCovarsPerRow=2,
+                               model="poisson")
+    cyclopsData <- convertToCyclopsData(outcomes = sim$outcomes,
+                                        covariates = sim$covariates,
+                                        modelType = "cpr",
+                                        addIntercept = FALSE)
+    fitCyclopsNoWeights <- fitCyclopsModel(cyclopsData = cyclopsData)
+    bsNoWeights <- runBootstrap(fitCyclopsNoWeights, replicates = 1999)
+    expect_lt(abs(mean(bsNoWeights$summary$bias)), 0.002)
+
+    sim$outcomes$weights <- rep(c(0.1,0.9), nrow(sim$outcomes) / 2)
+
+    cyclopsData <- convertToCyclopsData(outcomes = sim$outcomes,
+                                        covariates = sim$covariates,
+                                        modelType = "cpr",
+                                        addIntercept = FALSE)
+    fitCyclops <- fitCyclopsModel(cyclopsData = cyclopsData)
+    bs <- runBootstrap(fitCyclops, replicates = 1999)
+    expect_lt(abs(mean(bs$summary$bias)), 0.002)
+})
+
+test_that("Large Cox bootstrap with and without weights", {
+    set.seed(123)
+    sim <- simulateCyclopsData(nstrata=100,
+                               ncovars=4,
+                               nrows=1000,
+                               effectSizeSd=0.5,
+                               eCovarsPerRow=2,
+                               model="survival")
+    sim$outcomes$stratumId <- NULL
+    sim$covariates$stratumId <- NULL
+    cyclopsData <- convertToCyclopsData(outcomes = sim$outcomes,
+                                        covariates = sim$covariates,
+                                        modelType = "cox")
+    fitCyclopsNoWeights <- fitCyclopsModel(cyclopsData = cyclopsData)
+    bsNoWeights <- runBootstrap(fitCyclopsNoWeights, replicates = 1999)
+    expect_lt(abs(mean(bsNoWeights$summary$bias)), 0.002)
+
+    sim$outcomes$weights <- rep(c(0.1,0.9), nrow(sim$outcomes) / 2)
+
+    cyclopsData <- convertToCyclopsData(outcomes = sim$outcomes,
+                                        covariates = sim$covariates,
+                                        modelType = "cox")
+    fitCyclops <- fitCyclopsModel(cyclopsData = cyclopsData)
+    bs <- runBootstrap(fitCyclops, replicates = 1999)
+    expect_lt(abs(mean(bs$summary$bias)), 0.002)
+})
+
+test_that("Large conditional Cox bootstrap with and without weights", {
+    set.seed(123)
+    sim <- simulateCyclopsData(nstrata=100,
+                               ncovars=4,
+                               nrows=1000,
+                               effectSizeSd=0.5,
+                               eCovarsPerRow=2,
+                               model="survival")
+    cyclopsData <- convertToCyclopsData(outcomes = sim$outcomes,
+                                        covariates = sim$covariates,
+                                        modelType = "cox")
+    fitCyclopsNoWeights <- fitCyclopsModel(cyclopsData = cyclopsData)
+    bsNoWeights <- runBootstrap(fitCyclopsNoWeights, replicates = 1999)
+    expect_lt(abs(mean(bsNoWeights$summary$bias)), 0.002)
+
+    sim$outcomes$weights <- rep(c(0.1,0.9), nrow(sim$outcomes) / 2)
+
+    cyclopsData <- convertToCyclopsData(outcomes = sim$outcomes,
+                                        covariates = sim$covariates,
+                                        modelType = "cox")
+    fitCyclops <- fitCyclopsModel(cyclopsData = cyclopsData)
+    bs <- runBootstrap(fitCyclops, replicates = 1999)
+    expect_lt(abs(mean(bs$summary$bias)), 0.002)
+})
